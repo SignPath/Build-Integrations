e54e1fe74fa15d8db9ff1515760ec3f86c277fea
dominik.rauch <dominik.rauch@signpath.io>
2019-07-11 12:17:07 +0200

    Add AzureDevOps build integration.

---
+++ AzureDevOps/pack.ps1
@@ -0,0 +1,42 @@
+$MajorVersion = 0
+$MinorVersion = 2
+$PatchVersion = 1
+
+$tasks = @('signPathDownloadSignedArtifactTask', 'signPathSubmitSigningRequestTask')
+
+# Install VstsTaskSdk module for each task
+Write-Host "Installing VstsTaskSdk module..."
+$modulesDirectory = ".\ps_modules\"
+New-Item -ItemType Directory -Force -Path $modulesDirectory | Out-Null
+Save-Module -Name VstsTaskSdk -Path $modulesDirectory
+
+Write-Host "Copying VstsTaskSdk module to each task..."
+foreach($task in $tasks) {
+  $taskModulesDirectory = ".\$task\ps_modules\VstsTaskSdk\"
+  New-Item -ItemType Directory -Force -Path $taskModulesDirectory | Out-Null
+  Copy-Item -Force -Recurse "$($modulesDirectory)VstsTaskSdk\*\*" $taskModulesDirectory
+}
+
+# Set version for each task
+Write-Host "Setting version of each task to $MajorVersion.$MinorVersion.$PatchVersion..."
+foreach($task in $tasks) {
+  $taskDefinitionFile = ".\$task\task.json"
+  $json = Get-Content $taskDefinitionFile -raw | ConvertFrom-Json
+  $json.version.Major = $MajorVersion
+  $json.version.Minor = $MinorVersion
+  $json.version.Patch = $PatchVersion
+  $json | ConvertTo-Json -Depth 32 | Set-Content $taskDefinitionFile
+}
+
+# Set version for extension itself
+Write-Host "Setting extension version to $MajorVersion.$MinorVersion.$PatchVersion..."
+$extensionManifestFile = ".\vss-extension.json"
+$json = Get-Content $extensionManifestFile -raw | ConvertFrom-Json
+$json.version = "$MajorVersion.$MinorVersion.$PatchVersion"
+$json | ConvertTo-Json -Depth 32 | Set-Content $extensionManifestFile
+
+# Pack extension
+Write-Host "Packing extension..."
+tfx extension create --manifest-globs vss-extension.json
+
+Write-Host "Done."
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Build-Integrations
 
 SignPath integrations for various CI/CD systems.
+
+## Azure DevOps
+
+[Azure DevOps integration documentation](AzureDevOps/README.md)
