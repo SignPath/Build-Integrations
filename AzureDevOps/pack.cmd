a05b155caf069b2e28cad2fcb7791ca5775369fc
dominik.rauch <dominik.rauch@signpath.io>
2019-07-19 12:46:24 +0200

    Allow to explicitly set the version number via the command line.

--- AzureDevOps/pack.cmd
+++ AzureDevOps/pack.cmd
@@ -1,1 +1,4 @@
-powershell -File pack.ps1 -MajorVersion 0 -MinorVersion 0 -PatchVersion 1
+set /p major="Major version: "
+set /p minor="Minor version: "
+set /p patch="Patch version: "
+powershell -File pack.ps1 -MajorVersion %major% -MinorVersion %minor% -PatchVersion %patch%
